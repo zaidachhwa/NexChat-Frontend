385f3529fc04e202551cd52317eed2eee5cd48ca
zaidachhwa <zaidachhwa8486@gmail.com>
2025-12-15 13:05:27 +0530

    setup frontend

---
+++ src/app/login/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="bg-white p-5 min-h-screen w-full flex items-center justify-center">
+      {/* 1 */}
+      <div className="w-full bg-linear-to-tl from-icongreen to-fontgreen min-h-[90vh] rounded-2xl relative overflow-clip shadow-xl"></div>
+      {/* 2 */}
+      <div className="w-full min-h-full">
+        <form></form>
+      </div>
+    </div>
+  );
+};
+
+export default page;
